0afc6ab32026acccc2ab233aa5e19c7734b8f59c
pranjul soni <pranjulsoni1380@gmail.com>
2020-01-06 12:41:18 +0530

    first commit

---
+++ exercise_6.py
@@ -0,0 +1,9 @@
+input_username = input("Enter Your Name : ")
+input_age  = input("Enter Your age: ")
+input_age = int(input_age)
+
+
+if input_age == 10 and input_username[0] == 'h' or input_username[0] == 'H' :
+    print("You Win")
+else:
+    print("You Loss")
