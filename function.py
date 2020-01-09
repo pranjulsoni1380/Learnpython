1d993ba50904c82aa9be752f5686b87f848604ef
pranjul soni <pranjulsoni1380@gmail.com>
2020-01-09 20:00:02 +0530

    tutorial 72

---
+++ function.py
@@ -0,0 +1,15 @@
+# def add_two(a,b):
+#     return a+b
+
+# total = add_two(5,4)
+# print(total)
+
+# print(add_two(5,4))
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+def add_number(a,b):
+    return a+b
+
+print(add_number(num1,num2))
---
+++ function_2.py
@@ -0,0 +1,9 @@
+
+def odd_even(num):
+    if num%2 == 0:
+        print(num)
+        return "even"
+    else:
+        return "odd"
+    
+print(odd_even(9))
